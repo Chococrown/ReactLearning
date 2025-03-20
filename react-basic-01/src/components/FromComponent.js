e2262c16d19d1e7168587c33c9e5fa09e233df68
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-03-20 20:51:46 +0700

    Event

--- react-basic-01/src/components/FromComponent.js
+++ react-basic-01/src/components/FromComponent.js
@@ -1,21 +1,37 @@
+import "./FormComponent.css"
+
 const FormComponent = ()=>{
+
+    const inputTiltle = (event) =>{
+        console.log(event.target.value)
+    }
+
+    const inputAmount = (event) =>{
+        console.log(event.target.value)
+    }
+
+    const saveItem = (event) =>{
+        event.preventDefault()
+        console.log("บันทึกข้อมูลเรียบร้อย")
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit = {saveItem}>
                 <div className="form-Control">
                     <label>ชื่อรายการ</label>
-                    <input type="text" placholder="ระบุชื่อรายการของคุณ"/>
+                    <input type="text" placholder="ระบุชื่อรายการของคุณ" onChange={inputTiltle}/>
                 </div>
 
                 <div className="form-Control">
                 <label>จำนวนเงิน</label>
-                <input type="number" placholder="ระบุจำนวนเงิน"/>
+                <input type="number" placholder="ระบุจำนวนเงิน" onChange={inputAmount}/>
                 </div>
 
                 <div>
                     <button type="submit">เพิ่มข้อมูล</button>
                 </div>
-                
+
             </form>
         </div>
     )
