9a74a54e6f3beed4036418a4b4343eb58bc8d531
Chococrown <127465228+Chococrown@users.noreply.github.com>
2025-04-08 18:48:21 +0700

    เพิ่มรูปภาพ

---
+++ react-tese-02/test02/src/About.js
@@ -0,0 +1,15 @@
+import './about.css'
+
+function About() {
+    return <div class="about">
+        <div class="about__01">
+            <h2>เกี่ยวกับ</h2>
+            <br />
+            <img src="/images/profile.jpeg" alt="" style="width:180px; height: auto;" class="about__img__01" />            <div>สวัสดีครับผมชื่อประเสริฐ ตุ่นก่อ ที่สนใจด้านการพัฒนาเว็บแอปพลิเคชันด้วยเทคโนโลยีสมัยใหม่</div>
+            <div>ผมมุ่งมั่นที่จะพัฒนาเว็บแอปพลิเคชันด้วยเทคโนโลยีสมัยใหม่ให้มีความสวยงามและทันสมัย และพร้อมรับโอกาสใหม่ๆ
+                ในการพัฒนาทักษะและสร้างผลงานที่มีคุณค่า</div>
+        </div>
+    </div>
+}
+
+export default About;
---
+++ react-tese-02/test02/src/Header01.js
@@ -0,0 +1,17 @@
+import './header.css';
+
+function Header() {
+    return <header>
+        <div class="header__00">
+            <div class="header__01">ประเสริฐ ตุ่นก่อ</div>
+                <ul class="header__02">
+                    <div>หน้าหลัก</div>
+                    <div>ผลงาน</div>
+                    <div>ติดต่อ</div>
+                </ul>
+        </div>
+        
+    </header>
+}
+
+export default Header;
